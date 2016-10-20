5150258b87789aba2383ee42e333d50162008efc
Krystal Mejia <krystalmejia24@gmail.com>
2016-10-20 18:30:27 -0400

    added function to convert byte array to a bitset

--- MessageUtil.java
+++ MessageUtil.java
@@ -1,11 +1,12 @@
 import java.nio.ByteBuffer;
+import java.util.BitSet;
 
 /**
  * This class consists of useful functions related to messages, including conversions and parsing
- * 
+ *
  */
 public class MessageUtil {
-	
+
 	/**
 	 * Convert a single byte to an Integer
 	 * @param b:	the byte to be converted
@@ -14,7 +15,7 @@ public class MessageUtil {
 	public static int convertByteToInt(byte b) {
 		return (int) b;
 	}
-	
+
 	/**
 	 * Convert a sequence of bytes to an Integer
 	 * @param bytes:	the sequence of bytes to be converted
@@ -26,9 +27,24 @@ public class MessageUtil {
 		return i;
 	}
 
+	/**
+	 * Convert byter array to a bit set
+	 * @param bytes:	byte array
+	 * @return 			bitset
+	 */
+    public static BitSet convertToBitSet(byte[] bytes) {
+        BitSet bitset = new BitSet();
+        for (int i = 0; i < bytes.length * 8; i++) {
+            if ((bytes[bytes.length - i / 8 - 1] & (1 << (i % 8))) > 0) {
+                bitset.set(i);
+            }
+        }
+        return bitset;
+    }
+
 	// TODO: Implement Convert int to Byte/Bytes
-	
+
 	// TODO: Does the bitset contain pieces I don't have?
-	
+
 	// TODO: Extract piece from file
 }
