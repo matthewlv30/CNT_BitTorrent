8880468022b6b6286a4125cc343aedc39d368ae2
Allison Aguirre <allisonaguirre7106@gmail.com>
2016-12-06 11:57:22 -0500

    maputil

--- Unchoked.java
+++ Unchoked.java
@@ -124,6 +124,7 @@ public class Unchoked {
 			}
 			
 			// Sort the map of downloading rates
+			MapUtil a = new MapUtil();
 			downloadingRates = MapUtil.sortByValue(downloadingRates);
 			
 			it = downloadingRates.entrySet().iterator(); // Iterator for iterating through all the possible peers
